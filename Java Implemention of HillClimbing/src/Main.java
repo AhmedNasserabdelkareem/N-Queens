8ba8968daaf2691971a843cb1ae5860eb82ff3dc
Ahmed Nasser <ahmednasser2337@gmail.com>
2019-12-03 01:17:01 +0200

    Edit structure of project

--- Java Implemention of HillClimbing/src/Main.java
+++ Java Implemention of HillClimbing/src/Main.java
@@ -3,14 +3,20 @@ import model.State;
 import solver.HillClimbing;
 import utils.Reader;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
         Reader r = new Reader("src/test/Sample_Input.txt",8);
         //int[] initialBoard = r.read();
-        int [] initialBoard ={3,2,0,6,1,7,5,3};
+        //int [] initialBoard ={3,2,0,6,1,7,5,3};
+        int [] initialBoard = {3, 7, 4, 2, 0, 6, 1, 5};
         State initialState = new State(initialBoard);
+        System.out.println(initialState.getCost());
         initialState.showBoard();
         HillClimbing h = new HillClimbing(initialState);
         h.showResults();
+
     }
 }
